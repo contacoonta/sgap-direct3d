3a7e0690be7ceb5d40e4013e9ed3fd3481efa718
contacoonta <contacoonta@gmail.com>
2014-07-28 19:53:05 +0900

    edited event callback

--- 001 cpp extension/준비 events/00x Event Callback/EventSender.cpp
+++ 001 cpp extension/준비 events/00x Event Callback/EventSender.cpp
@@ -5,6 +5,22 @@ void EventSender::RegisterListener(iCallback* pcb)
 	cbList.push_back(pcb);
 }
 
+void EventSender::UnregisterListener(iCallback** ppcb)
+{
+	for (auto* x : cbList)
+	{
+		if (x == *ppcb)
+		{
+			cbList.remove(*ppcb);
+
+			delete *ppcb;
+			*ppcb = nullptr;
+
+			return;
+		}
+	}
+}
+
 void EventSender::Invoke(void * pdat)
 {
 	if (cbList.empty())
--- 001 cpp extension/준비 events/00x Event Callback/EventSender.h
+++ 001 cpp extension/준비 events/00x Event Callback/EventSender.h
@@ -11,8 +11,20 @@ class EventSender
 {
 public:
 	void RegisterListener(iCallback*);
+	void UnregisterListener(iCallback**);
 
 	void Invoke(void * pdat);
+
+
+	void operator += (iCallback* pcb)
+	{
+		RegisterListener(pcb);
+	}
+
+	void operator -= (iCallback** ppcb)
+	{
+		UnregisterListener(ppcb);
+	}
 
 private:
 	list<iCallback*> cbList;
