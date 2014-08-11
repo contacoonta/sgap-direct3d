39379cd8fefe9202ed104dc3b55c5911da4d806f
contacoonta <contacoonta@gmail.com>
2014-08-11 22:25:21 +0900

    edit device initialize

--- 003 d3d rough initialize/sourcecode/01 device initialize/initialize.cpp
+++ 003 d3d rough initialize/sourcecode/01 device initialize/initialize.cpp
@@ -1,224 +1,228 @@
 /*
-	D3d11 
-	- Device			생성
-	- Device Context	생성
-	- Swap Chain		생성
-	- Render Target		생성
+	d3d device initialize
+
+	- Device
+		: D3d 를 총괄하는 인터페이스
+	- Device Context
+		: 그려주기위한 인터페이스
+	- Swap Chain
+		: Page Flipping , 더블버퍼링
+	- Render Target View
+		: 그릴 영역
 */
+
 #include <windows.h>
 #include <d3d11.h>
 #include <d3dx11.h>
-#include "resource.h"
 
-
-//--------------------------------------------------------------------------------------
-// Global Variables
-//--------------------------------------------------------------------------------------
 HINSTANCE               g_hInst = NULL;
 HWND                    g_hWnd = NULL;
-D3D_DRIVER_TYPE         g_driverType = D3D_DRIVER_TYPE_NULL;
-D3D_FEATURE_LEVEL       g_featureLevel = D3D_FEATURE_LEVEL_11_0;
-ID3D11Device*           g_pd3dDevice = NULL;
-ID3D11DeviceContext*    g_pImmediateContext = NULL;
-IDXGISwapChain*         g_pSwapChain = NULL;
-ID3D11RenderTargetView* g_pRenderTargetView = NULL;
+
+ID3D11Device*           g_device = NULL;
+ID3D11DeviceContext*    g_devcon = NULL;
+IDXGISwapChain*         g_swapchain = NULL;
+ID3D11RenderTargetView* g_rtview = NULL;
 
 
-//--------------------------------------------------------------------------------------
-// Forward declarations
-//--------------------------------------------------------------------------------------
-HRESULT InitDevice();
-LRESULT CALLBACK    WndProc( HWND, UINT, WPARAM, LPARAM );
+
+HRESULT		InitDevice();
+void		ReleaseDevice();
 
 
-//--------------------------------------------------------------------------------------
-// Entry point to the program. Initializes everything and goes into a message processing 
-// loop. Idle time is used to render the scene.
-//--------------------------------------------------------------------------------------
-int WINAPI wWinMain( HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow )
+/*
+	윈도 메시지 프로시져
+*/
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER( hPrevInstance );
-    UNREFERENCED_PARAMETER( lpCmdLine );
+	switch (message)
+	{
+	case WM_CLOSE:
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
 
-	 // Register class
-    WNDCLASSEX wcex;
-    wcex.cbSize = sizeof( WNDCLASSEX );
-    wcex.style = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc = WndProc;
-    wcex.cbClsExtra = 0;
-    wcex.cbWndExtra = 0;
-    wcex.hInstance = hInstance;
-    wcex.hIcon = LoadIcon( hInstance, ( LPCTSTR )IDI_TUTORIAL1 );
-    wcex.hCursor = LoadCursor( NULL, IDC_ARROW );
-    wcex.hbrBackground = ( HBRUSH )( COLOR_WINDOW + 1 );
-    wcex.lpszMenuName = NULL;
-    wcex.lpszClassName = L"001 Basics";
-    wcex.hIconSm = LoadIcon( wcex.hInstance, ( LPCTSTR )IDI_TUTORIAL1 );
-    if( !RegisterClassEx( &wcex ) )
-        return E_FAIL;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
 
-    // Create window
-    g_hInst = hInstance;
-    RECT rc = { 0, 0, 640, 480 };
-    AdjustWindowRect( &rc, WS_OVERLAPPEDWINDOW, FALSE );
-    g_hWnd = CreateWindow( L"001 Basics", L"Direct3D 11 Initialize", WS_OVERLAPPEDWINDOW,
-                           CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance,
-                           NULL );
-    if( !g_hWnd )
-        return E_FAIL;
+	return 0;
+}
 
-    ShowWindow( g_hWnd, nCmdShow );
+/*
+	윈 메인
+*/
+int WINAPI wWinMain(HINSTANCE hInstance, 
+	HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
+{
+	wchar_t appname[50] = L"001 Basics";
 
+	// 윈도우 구조체 채우기
+	WNDCLASSEX wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, (LPCTSTR)IDI_WINLOGO);
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = appname;
+	wcex.hIconSm = wcex.hIcon;
+	if (!RegisterClassEx(&wcex))
+		return E_FAIL;
 
-    if( FAILED( InitDevice() ) )
-    {
-		if (g_pImmediateContext) g_pImmediateContext->ClearState();
+	// 윈도우 생성
+	g_hInst = hInstance;
+	RECT rc = { 0, 0, 1024, 768 };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
+	g_hWnd = CreateWindow(appname, appname, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, rc.right - rc.left, rc.bottom - rc.top, 
+		NULL, NULL, hInstance, NULL);
 
-		if (g_pRenderTargetView) g_pRenderTargetView->Release();
-		if (g_pSwapChain) g_pSwapChain->Release();
-		if (g_pImmediateContext) g_pImmediateContext->Release();
-		if (g_pd3dDevice) g_pd3dDevice->Release();
-        return 0;
-    }
+	// 윈도 출력
+	ShowWindow(g_hWnd, nCmdShow);
 
-    // Main message loop
-    MSG msg = {0};
-    while( WM_QUIT != msg.message )
-    {
-        if( PeekMessage( &msg, NULL, 0, 0, PM_REMOVE ) )
-        {
-            TranslateMessage( &msg );
-            DispatchMessage( &msg );
-        }
-        else
-        {
-			// Just clear the backbuffer
-			float ClearColor[4] = { 0.0f, 0.125f, 0.3f, 1.0f }; //red,green,blue,alpha
-			g_pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, ClearColor);
-			g_pSwapChain->Present(0, 0);
-        }
-    }
+	// d3d device 생성
+	InitDevice();
 
-	if (g_pImmediateContext) g_pImmediateContext->ClearState();
+	// 기본 메시지 루프
+	MSG msg = { 0 };
+	while (WM_QUIT != msg.message)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else //MainLoop
+		{
+			// 업데이트..
 
-	if (g_pRenderTargetView) g_pRenderTargetView->Release();
-	if (g_pSwapChain) g_pSwapChain->Release();
-	if (g_pImmediateContext) g_pImmediateContext->Release();
-	if (g_pd3dDevice) g_pd3dDevice->Release();
+			// 백버퍼 지우기
+			float ClearColor[4] = { 0.0f, 0.125f, 0.3f, 1.0f };
+			g_devcon->ClearRenderTargetView(g_rtview, ClearColor);
+			
+			
+			// 렌더링..
 
-    return ( int )msg.wParam;
+			
+			// 더블 버퍼링
+			g_swapchain->Present(0, 0);
+		}
+	}//while
+
+	// D3d device 들 해제
+	ReleaseDevice();
+
+	return (int)msg.wParam;
 }
 
 
-
-//--------------------------------------------------------------------------------------
-// Called every time the application receives a message
-//--------------------------------------------------------------------------------------
-LRESULT CALLBACK WndProc( HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam )
+HRESULT		InitDevice()
 {
-    PAINTSTRUCT ps;
-    HDC hdc;
+	HRESULT hr = S_OK;
 
-    switch( message )
-    {
-        case WM_PAINT:
-            hdc = BeginPaint( hWnd, &ps );
-            EndPaint( hWnd, &ps );
-            break;
+	RECT rc;
+	GetClientRect(g_hWnd, &rc);
+	UINT width = rc.right - rc.left;
+	UINT height = rc.bottom - rc.top;
 
-        case WM_DESTROY:
-            PostQuitMessage( 0 );
-            break;
-
-        default:
-            return DefWindowProc( hWnd, message, wParam, lParam );
-    }
-
-    return 0;
-}
-
-
-//--------------------------------------------------------------------------------------
-// Create Direct3D device and swap chain
-//--------------------------------------------------------------------------------------
-HRESULT InitDevice()
-{
-    HRESULT hr = S_OK;
-
-    RECT rc;
-    GetClientRect( g_hWnd, &rc );
-    UINT width = rc.right - rc.left;
-    UINT height = rc.bottom - rc.top;
-
-    UINT createDeviceFlags = 0;
+	UINT createDeviceFlags = 0;
 #ifdef _DEBUG
-    createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
+	createDeviceFlags |= D3D11_CREATE_DEVICE_DEBUG;
 #endif
 
-    D3D_DRIVER_TYPE driverTypes[] =
-    {
-        D3D_DRIVER_TYPE_HARDWARE,
-        D3D_DRIVER_TYPE_WARP,
-        D3D_DRIVER_TYPE_REFERENCE,
-    };
-    UINT numDriverTypes = ARRAYSIZE( driverTypes );
+	D3D_DRIVER_TYPE driverTypes[] =
+	{
+		D3D_DRIVER_TYPE_HARDWARE,
+		D3D_DRIVER_TYPE_WARP,
+		D3D_DRIVER_TYPE_REFERENCE,
+	};
+	UINT numDriverTypes = ARRAYSIZE(driverTypes);
 
-    D3D_FEATURE_LEVEL featureLevels[] =
-    {
-        D3D_FEATURE_LEVEL_11_0,
-        D3D_FEATURE_LEVEL_10_1,
-        D3D_FEATURE_LEVEL_10_0,
-    };
-	UINT numFeatureLevels = ARRAYSIZE( featureLevels );
+	D3D_FEATURE_LEVEL featureLevels[] =
+	{
+		D3D_FEATURE_LEVEL_11_0,
+		D3D_FEATURE_LEVEL_10_1,
+		D3D_FEATURE_LEVEL_10_0,
+	};
+	UINT numFeatureLevels = ARRAYSIZE(featureLevels);
 
-    DXGI_SWAP_CHAIN_DESC sd;
-    ZeroMemory( &sd, sizeof( sd ) );
-    sd.BufferCount = 1;
-    sd.BufferDesc.Width = width;
-    sd.BufferDesc.Height = height;
-    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-    sd.BufferDesc.RefreshRate.Numerator = 60;
-    sd.BufferDesc.RefreshRate.Denominator = 1;
-    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    sd.OutputWindow = g_hWnd;
-    sd.SampleDesc.Count = 1;
-    sd.SampleDesc.Quality = 0;
-    sd.Windowed = TRUE;
+	// 백버퍼에 대한 설정
+	DXGI_SWAP_CHAIN_DESC sd;
+	ZeroMemory(&sd, sizeof(sd));
+	sd.BufferCount = 1;
+	sd.BufferDesc.Width = width;
+	sd.BufferDesc.Height = height;
+	sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+	sd.BufferDesc.RefreshRate.Numerator = 60;
+	sd.BufferDesc.RefreshRate.Denominator = 1;
+	sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+	sd.OutputWindow = g_hWnd;
+	sd.SampleDesc.Count = 1;
+	sd.SampleDesc.Quality = 0;
+	sd.Windowed = TRUE;
+
+	D3D_DRIVER_TYPE		driverType = D3D_DRIVER_TYPE_NULL;
+	D3D_FEATURE_LEVEL	featureLevel = D3D_FEATURE_LEVEL_11_0;
+
+	for (UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; 
+														driverTypeIndex++)
+	{
+		driverType = driverTypes[driverTypeIndex];
+
+		// Device , Device Context , SwapChain 을 동시에 생성한다.
+		hr = D3D11CreateDeviceAndSwapChain(NULL, driverType, NULL, 
+							createDeviceFlags, featureLevels, numFeatureLevels,
+							D3D11_SDK_VERSION, &sd, 
+							&g_swapchain, &g_device, &featureLevel, &g_devcon);
+
+		if (SUCCEEDED(hr))
+			break;
+	}
+	if (FAILED(hr))
+		return hr;
+
+	/*
+		렌더 타겟 뷰 생성
+	*/
+	ID3D11Texture2D* pBackBuffer = NULL;
+	hr = g_swapchain->GetBuffer(0, __uuidof(ID3D11Texture2D), 
+											(LPVOID*)&pBackBuffer);
+	if (FAILED(hr))
+		return hr;
+
+	hr = g_device->CreateRenderTargetView(pBackBuffer, NULL, &g_rtview);
+	pBackBuffer->Release();
+	if (FAILED(hr))
+		return hr;
+
+	// device context 에 렌더타겟 연결
+	g_devcon->OMSetRenderTargets(1, &g_rtview, NULL);
 
 
-    for( UINT driverTypeIndex = 0; driverTypeIndex < numDriverTypes; driverTypeIndex++ )
-    {
-        g_driverType = driverTypes[driverTypeIndex];
-        hr = D3D11CreateDeviceAndSwapChain( NULL, g_driverType, NULL, createDeviceFlags, featureLevels, numFeatureLevels,
-                                            D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &g_featureLevel, &g_pImmediateContext );
-        if( SUCCEEDED( hr ) )
-            break;
-    }
-    if( FAILED( hr ) )
-        return hr;
+	// Viewport 세팅
+	D3D11_VIEWPORT vp;
+	vp.Width = (FLOAT)width;
+	vp.Height = (FLOAT)height;
+	vp.MinDepth = 0.0f;
+	vp.MaxDepth = 1.0f;
+	vp.TopLeftX = 0;
+	vp.TopLeftY = 0;
 
-    // Create a render target view
-    ID3D11Texture2D* pBackBuffer = NULL;
-    hr = g_pSwapChain->GetBuffer( 0, __uuidof( ID3D11Texture2D ), ( LPVOID* )&pBackBuffer );
-    if( FAILED( hr ) )
-        return hr;
+	g_devcon->RSSetViewports(1, &vp);
 
-    hr = g_pd3dDevice->CreateRenderTargetView( pBackBuffer, NULL, &g_pRenderTargetView );
-    pBackBuffer->Release();
-    if( FAILED( hr ) )
-        return hr;
+	return S_OK;
+}
 
-    g_pImmediateContext->OMSetRenderTargets( 1, &g_pRenderTargetView, NULL );
+void ReleaseDevice()
+{
+	if (g_devcon)		g_devcon->ClearState();
 
-    // Setup the viewport
-    D3D11_VIEWPORT vp;
-    vp.Width = (FLOAT)width;
-    vp.Height = (FLOAT)height;
-    vp.MinDepth = 0.0f;
-    vp.MaxDepth = 1.0f;
-    vp.TopLeftX = 0;
-    vp.TopLeftY = 0;
-    g_pImmediateContext->RSSetViewports( 1, &vp );
-
-    return S_OK;
+	if (g_rtview)		g_rtview->Release();
+	if (g_swapchain)	g_swapchain->Release();
+	if (g_devcon)		g_devcon->Release();
+	if (g_device)		g_device->Release();
 }
